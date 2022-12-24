c9da805c74a44fba76f3cb5dbaa75b539d2647a4
Chaitalimane2027 <114143495+Chaitalimane2027@users.noreply.github.com>
2022-12-24 23:35:14 +0530

    Create routes.ts

---
+++ src/app/routes.ts
@@ -0,0 +1,14 @@
+import { Routes } from '@angular/router'
+import { HomeDetailsComponent } from './home/home.component'
+import { ViewInvoiceComponent } from './view-invoice/view-invoice.component'
+import { LoginComponent } from './login/login.component'
+import { PolicyListComponent } from './policy-details/policy-list.component'
+
+
+export const appRoutes: Routes = [
+  { path: 'home', component: HomeDetailsComponent },
+  { path: '', component: HomeDetailsComponent },
+  { path: 'policydetails', component: PolicyListComponent},
+  { path: 'viewinvoice', component: ViewInvoiceComponent },
+  { path: 'login', component: LoginComponent }
+]
